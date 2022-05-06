ea264f212989bf732ea8f5b7262689533b37d010
Angela Alarcon <aangelaabril101@gmail.com>
2022-05-06 10:13:30 -0400

    primer componente

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { cardbottom } from './cardbottom';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <section className="App">
+     <cardbottom></cardbottom>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/cardbottom.jsx
@@ -0,0 +1,21 @@
+
+import '../styles/cardbottom.css'
+
+import React from 'react'
+
+function cardbottom() {
+  return (
+    <div className='cardbottom'>
+        <div className="description">
+        Description of what is done here
+        </div>
+        <div className="last-updated">
+        <span>Last Updated</span>
+        <span>3/8/2020</span>
+        </div>
+    </div>
+    
+  )
+}
+
+export {cardbottom} 
